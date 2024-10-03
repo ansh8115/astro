9bb986f64511a0597320ad29ab7fa9eb694a94ab
ansh8115 <ashu.pathak8115@gmail.com>
2024-10-03 13:25:35 +0530

    Add files via upload

---
+++ astrologer.py
@@ -0,0 +1,9 @@
+str1=input("enter the husbend  name :")
+str2=input("enter the wife name:")
+len1= len(str1)
+len2=len(str2)
+sum= (len1+len2)
+if sum%2==0:
+    print("husbend is first dead")
+else:
+    print("wife is first dead")
